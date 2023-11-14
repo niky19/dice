c8f0f9661fa7b8c5d8e76baf6bcf4f243620d6a2
niky19 <92981186+niky19@users.noreply.github.com>
2023-11-14 09:35:19 +0100

    Fitxers inicials per al projecte MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(1, 6))
+    if (true) {
+        basic.showIcon(IconNames.Heart)
+    }
+})
